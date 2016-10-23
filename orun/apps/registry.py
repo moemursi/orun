379e41ba875ef8362384b0465ad9a8b19bf59b5d
alexandre <alexandre@katrid.com>
2016-10-23 19:47:15 -0200

    Initial commit

--- orun/apps/registry.py
+++ orun/apps/registry.py
@@ -32,6 +32,7 @@ class Registry(object):
         base_dir = os.path.join(os.path.dirname(__file__))
         self.addon_path = [os.path.join(base_dir, '..', 'addons'), os.path.join(base_dir, '..', '..', 'addons')]  # basic addons paths
         if ADDONS_ENVIRONMENT_VARIABLE in os.environ:
+            print('ADDONS PATH', os.environ[ADDONS_ENVIRONMENT_VARIABLE].split(';'))
             self.addon_path.extend(os.environ[ADDONS_ENVIRONMENT_VARIABLE].split(';'))
 
     def get_model(self, app_label, model_name=None):
