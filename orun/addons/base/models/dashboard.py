bfd12ec6b414f5933b8f6b2c019034f31aa49049
katrid <alexandre@katrid.com>
2018-05-18 22:15:34 -0300

    Initial commit

--- orun/addons/base/models/dashboard.py
+++ orun/addons/base/models/dashboard.py
@@ -1,6 +1,7 @@
+from flask import request
 from orun.db import models, session
 from orun.utils.translation import gettext_lazy as _
-from orun import api
+from orun import api, g
 
 
 class Category(models.Model):
@@ -24,9 +25,20 @@ class Query(models.Model):
     def get_by_natural_key(self, category, name):
         return self.objects.filter({'category': category, 'name': name}).one()
 
+    def _prepare_params(self):
+        ctx = {
+            'request': request,
+            'user_id': self.env.user_id,
+            'user': self.env.user,
+        }
+        # evaluate query params
+        params = eval(self.params, ctx)
+
     @api.method
     def read(self, id, with_desc=False, **kwargs):
-        q = session.execute(self.objects.get(id).sql)
+        q = self.objects.get(id)
+        params = q._prepare_params()
+        q = session.execute(q.sql, params)
         desc = q.cursor.description
         if with_desc:
             fields = [{'field': f[0], 'type': f[1], 'size': f[2]} for f in desc]
