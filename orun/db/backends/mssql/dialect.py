0d1bef11eec6e7684fb1c0234fc2cddb47386a1a
alexandre <alexandre@katrid.com>
2018-07-16 15:34:05 -0300

    Initial commit

--- orun/db/backends/mssql/dialect.py
+++ orun/db/backends/mssql/dialect.py
@@ -27,17 +27,17 @@ class MSSQLDialect(sqlalchemy.dialects.mssql.pyodbc.dialect):
 
     @sqlalchemy.dialects.mssql.base._db_plus_owner
     def get_foreign_keys(self, connection, tablename, dbname, owner, schema, **kw):
+        if schema:
+            tablename = schema + '.' + tablename
         sql = '''select fk.name constraint_name, cols.name as column_name, schemas.name as schema_name, rt.name as table_name
 from sys.foreign_keys fk
 inner join sys.tables rt on fk.referenced_object_id = rt.object_id
 inner join sys.foreign_key_columns fkc on fkc.constraint_object_id = fk.object_id
 inner join sys.columns cols on cols.object_id = fk.parent_object_id and cols.column_id = fkc.parent_column_id
 inner join sys.schemas schemas on schemas.schema_id = rt.schema_id
-where fk.parent_object_id = OBJECT_ID(:table)
-'''
-        if schema:
-            tablename = schema + '.' + tablename
-        for fk in connection.execute(sql, {'table': tablename}).fetchall():
+where fk.parent_object_id = OBJECT_ID('%s')
+''' % tablename
+        for fk in connection.execute(sql).fetchall():
             yield {
                 'name': fk.constraint_name,
                 'constrained_columns': [fk.column_name],
--- orun/db/backends/mssql/schema.py
+++ orun/db/backends/mssql/schema.py
@@ -10,7 +10,7 @@ class DatabaseSchemaEditor(BaseDatabaseSchemaEditor):
     sql_alter_column_not_null = "ALTER COLUMN %(column)s %(definition)s NULL"
     sql_set_sequence_max = "DBCC CHECKIDENT ('%(table)s')"
     sql_rename_table = "SP_RENAME '%(old_table)s', '%(new_table)s'"
-    sql_rename_column = "SP_RENAME '%(table)s.%(old_column)s', %(new_column)s, 'COLUMN'"
+    sql_rename_column = "EXEC SP_RENAME '%(table)s.%(old_column)s', '%(new_column)s', 'COLUMN'"
     sql_delete_column = "ALTER TABLE %(table)s DROP COLUMN %(column)s"
     sql_delete_table = "DROP TABLE %(table)s"
 
