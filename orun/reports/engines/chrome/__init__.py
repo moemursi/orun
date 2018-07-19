7fa579e94f4db68ba2206ea92b4cc97dd9766567
alexandre <alexandre@katrid.com>
2018-07-19 14:21:57 -0300

    Initial commit

---
+++ orun/reports/engines/chrome/__init__.py
@@ -0,0 +1,10 @@
+
+
+class ReportEngine:
+    def from_xml(xml):
+        pass
+
+
+if __name__ == '__main__':
+    rep = ReportEngine()
+    rep.from_xml()
---
+++ orun/reports/engines/chrome/static/reports.js
@@ -0,0 +1,12 @@
+
+
+angular.module('reports.katrid')
+.directive('report', () => ({
+  restrict: 'E',
+  link: function (scope, el, attrs) {
+
+  },
+  template: function (el, attrs) {
+    
+  }
+}));
