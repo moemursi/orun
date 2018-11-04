7f8181c074175610e0ac8adec8fab784f3e4dbfc
alexandre <alexandre@katrid.com>
2018-11-04 17:18:22 -0200

    Initial commit

--- orun/reports/engines/chrome/utils.py
+++ orun/reports/engines/chrome/utils.py
@@ -1,5 +1,18 @@
+from statistics import mean
 
 
 def to_values(iterable, member):
     for obj in iterable:
         yield getattr(obj, member)
+
+
+def avg(iterable, member):
+    vals = list(to_values(iterable, member))
+    if vals:
+        return mean(vals)
+
+
+def total(iterable, member):
+    vals = to_values(iterable, member)
+    return sum(vals)
+
