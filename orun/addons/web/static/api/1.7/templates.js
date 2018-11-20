92bc6c8f17e2dde5833e850532956d25d0362e61
alexandre <alexandre@katrid.com>
2018-11-20 17:59:59 -0200

    Initial commit

--- orun/addons/web/static/api/1.7/templates.js
+++ orun/addons/web/static/api/1.7/templates.js
@@ -1095,7 +1095,7 @@ function pug_classes_object(r){var a="",n="";for(var o in r)o&&r[o]&&pug_has_own
 function pug_escape(e){var a=""+e,t=pug_match_html.exec(a);if(!t)return e;var r,c,n,s="";for(r=t.index,c=0;r<a.length;r++){switch(a.charCodeAt(r)){case 34:n="&quot;";break;case 38:n="&amp;";break;case 60:n="&lt;";break;case 62:n="&gt;";break;default:continue}c!==r&&(s+=a.substring(c,r)),c=r+1,s+=n}return c!==r?s+a.substring(c,r):s}
 var pug_has_own_property=Object.prototype.hasOwnProperty;
 var pug_match_html=/["&<>]/;
-function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (field, inplaceEditor, name) {;pug_debug_line = 1;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+function pug_rethrow(n,e,r,t){if(!(n instanceof Error))throw n;if(!("undefined"==typeof window&&e||t))throw n.message+=" on line "+r,n;try{t=t||require("fs").readFileSync(e,"utf8")}catch(e){pug_rethrow(n,null,r)}var i=3,a=t.split("\n"),o=Math.max(r-i,0),h=Math.min(a.length,r+i),i=a.slice(o,h).map(function(n,e){var t=e+o+1;return(t==r?"  > ":"    ")+t+"| "+n}).join("\n");throw n.path=e,n.message=(e||"Pug")+":"+r+"\n"+i+"\n\n"+n.message,n}function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;var pug_debug_filename, pug_debug_line;try {;var locals_for_with = (locals || {});(function (field, inplaceEditor) {;pug_debug_line = 1;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
 ;pug_debug_line = 2;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
 var cssClass = 'StringField'
 ;pug_debug_line = 3;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
@@ -1120,14 +1120,22 @@ else {
 ;pug_debug_line = 12;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
 ;pug_debug_line = 13;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
 pug_html = pug_html + "\u003Cspan\u003E";
-;pug_debug_line = 13;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
-pug_html = pug_html + "{{ record.";
-;pug_debug_line = 13;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
-pug_html = pug_html + (pug_escape(null == (pug_interp = name) ? "" : pug_interp));
-;pug_debug_line = 13;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
-pug_html = pug_html + " }}\u003C\u002Fspan\u003E";
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + "{{ view.fields['";
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = field.name) ? "" : pug_interp));
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + "'].displayChoices[record.";
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = field.name) ? "" : pug_interp));
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + "] || '";
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + (pug_escape(null == (pug_interp = field.emptyText) ? "" : pug_interp));
+;pug_debug_line = 14;pug_debug_filename = "src\u002Ftemplates\u002Fview.list.selection-field.pug";
+pug_html = pug_html + "' }}\u003C\u002Fspan\u003E";
 }
-pug_html = pug_html + "\u003C\u002Ftd\u003E";}.call(this,"field" in locals_for_with?locals_for_with.field:typeof field!=="undefined"?field:undefined,"inplaceEditor" in locals_for_with?locals_for_with.inplaceEditor:typeof inplaceEditor!=="undefined"?inplaceEditor:undefined,"name" in locals_for_with?locals_for_with.name:typeof name!=="undefined"?name:undefined));} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line);};return pug_html;}
+pug_html = pug_html + "\u003C\u002Ftd\u003E";}.call(this,"field" in locals_for_with?locals_for_with.field:typeof field!=="undefined"?field:undefined,"inplaceEditor" in locals_for_with?locals_for_with.inplaceEditor:typeof inplaceEditor!=="undefined"?inplaceEditor:undefined));} catch (err) {pug_rethrow(err, pug_debug_filename, pug_debug_line);};return pug_html;}
   return template;
 })());
 
