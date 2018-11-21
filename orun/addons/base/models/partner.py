cef65dbf21c9346e740a566c5c7bb0275d1aad61
alexandre <alexandre@katrid.com>
2018-11-21 16:24:34 -0200

    Initial commit

--- orun/addons/base/models/partner.py
+++ orun/addons/base/models/partner.py
@@ -64,10 +64,6 @@ class Partner(models.Model):
         verbose_name = _('Partner')
         verbose_name_plural = _('Partners')
 
-    def save(self, *args, **kwargs):
-        self.company_id = 2
-        super().save(*args, **kwargs)
-
     def __str__(self):
         return self.name
 
